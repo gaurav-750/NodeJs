9db27d1aad458009505824ac93950caca99aa066
Gaurav <gauravsomani52750@gmail.com>
2024-01-19 18:10:11 +0530

    created a Product model

--- ExpressIntro/controllers/products.js
+++ ExpressIntro/controllers/products.js
@@ -1,4 +1,5 @@
-const products = [];
+const Product = require("../models/product");
+// const products = [];
 
 //* controller for get add product
 exports.getAddProducts = (req, res, next) => {
@@ -10,12 +11,16 @@ exports.getAddProducts = (req, res, next) => {
 
 exports.postAddProduct = (req, res, next) => {
   console.log(req.body);
-  products.push({ title: req.body.title });
+  //   products.push({ title: req.body.title });
+  const newProduct = new Product(req.body.title);
+  newProduct.save();
 
   res.redirect("/");
 };
 
 exports.getProducts = (req, res, next) => {
+  const products = Product.fetchAllProducts();
+
   //rendering ejs file -> shop.pug
   res.render("shop", {
     prods: products, //passing data to ejs file
