a0d2380c87da8e788ffe6aabfeac99d2983d1cdc
Gaurav <gauravsomani52750@gmail.com>
2024-01-23 18:44:22 +0530

    updated admin controllers -> using sequelize

--- ExpressIntro/controllers/admin.js
+++ ExpressIntro/controllers/admin.js
@@ -1,4 +1,19 @@
+const { where } = require("sequelize");
 const Product = require("../models/product");
+
+exports.getAllProducts = (req, res, next) => {
+  Product.findAll()
+    .then((products) => {
+      res.render("admin/products", {
+        prods: products, //passing data to ejs file
+        pageTitle: "Admin Products",
+        path: "/admin/products",
+      });
+    })
+    .catch((err) => {
+      console.log("[Controllers/Admin/getAllProducts] err:", err);
+    });
+};
 
 //* controller for get add product
 exports.getAddProducts = (req, res, next) => {
@@ -21,6 +36,7 @@ exports.postAddProduct = (req, res, next) => {
     .then((result) => {
       // console.log("result in postAddProduct:", result);
       console.log("[Controllers/Admin/postAddProduct]: Created Product ");
+      res.redirect("/admin/products");
     })
     .catch((err) => {
       console.log("err in postAddProduct:", err);
@@ -28,60 +44,74 @@ exports.postAddProduct = (req, res, next) => {
 };
 
 exports.getEditProduct = (req, res, next) => {
+  console.log("[Controllers/Admin/getEditProduct] req.query:", req.query);
   const editMode = req.query.edit;
   if (!editMode) {
     return res.redirect("/");
   }
 
   const { productId } = req.params;
-  Product.findProductById(productId, (product) => {
-    if (!product) {
-      return res.redirect("/");
-    }
+  Product.findByPk(productId)
+    .then((product) => {
+      if (!product) {
+        return res.redirect("/");
+      }
 
-    //if product exist
-    res.render("admin/edit-product", {
-      pageTitle: "Edit Product",
-      path: "/admin/edit-product",
-      editMode,
-      product,
+      //if product exist
+      res.render("admin/edit-product", {
+        pageTitle: "Edit Product",
+        path: "/admin/edit-product",
+        editMode,
+        product,
+      });
+    })
+    .catch((err) => {
+      console.log("[Controllers/Admin/getEditProduct] err:", err);
     });
-  });
 };
 
 exports.postEditProduct = (req, res, next) => {
-  //update the existing product
-  const productId = req.body.productId;
+  console.log("[Controllers/Admin/postEditProduct] req.body:", req.body);
   const { title, imageUrl, price, description } = req.body;
 
-  const updatedProduct = new Product(
-    productId,
-    title,
-    imageUrl,
-    description,
-    price
-  );
-
-  updatedProduct.save();
-  res.redirect("/admin/products");
+  const productId = req.body.productId;
+  Product.update(
+    {
+      title,
+      price,
+      imageUrl,
+      description,
+    },
+    {
+      //where clause
+      where: {
+        id: productId,
+      },
+    }
+  )
+    .then((result) => {
+      console.log("[Controllers/Admin/postEditProduct] Product Updated.");
+      res.redirect("/admin/products");
+    })
+    .catch((err) => {
+      console.log("[Controllers/Admin/postEditProduct] err:", err);
+    });
 };
 
 exports.postDeleteProduct = (req, res, next) => {
+  console.log("[Controllers/Admin/postDeleteProduct] req.body:", req.body);
   const { productId } = req.body;
-  Product.deleteById(productId);
 
-  res.redirect("/admin/products");
-};
-
-exports.getAllProducts = (req, res, next) => {
-  Product.fetchAllProducts((products) => {
-    console.log("[Controllers/Admin]: getAllProducts", products.length);
-
-    //rendering ejs file -> shop.pug
-    res.render("admin/products", {
-      prods: products, //passing data to ejs file
-      pageTitle: "Admin Products",
-      path: "/admin/products",
+  Product.destroy({
+    where: {
+      id: productId,
+    },
+  })
+    .then((result) => {
+      console.log("[Controllers/Admin/postDeleteProduct] Product Deleted.");
+      res.redirect("/admin/products");
+    })
+    .catch((err) => {
+      console.log("[Controllers/Admin/postDeleteProduct] err:", err);
     });
-  });
 };
